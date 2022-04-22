a58f43c46c6ac8aaa7a2e72084ec1033f26ca278
Jonas <jonasexplore@gmail.com>
2022-04-22 10:40:01 -0300

    feat: validate local and domain

--- src/entities/Email.ts
+++ src/entities/Email.ts
@@ -1,5 +1,22 @@
 export class Email {
   static validate(email: string): boolean {
-    return email ? true : false;
+    if (!email) {
+      return false;
+    }
+
+    if (email.length > 320) {
+      return false;
+    }
+
+    const [local, domain] = email.split("@");
+    if (local.length > 64) {
+      return false;
+    }
+
+    if (domain.length > 255) {
+      return false;
+    }
+
+    return true;
   }
 }
