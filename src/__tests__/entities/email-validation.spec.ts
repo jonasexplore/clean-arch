7db0eab1c9ef77f51ce7a6efb1ab3973b5e335b2
Jonas <jonasexplore@gmail.com>
2022-04-22 10:40:22 -0300

    test: validate local and domain

--- src/__tests__/entities/email-validation.spec.ts
+++ src/__tests__/entities/email-validation.spec.ts
@@ -15,4 +15,19 @@ describe("Entities > Email validation", () => {
     const email = "email@email.com";
     expect(Email.validate(email)).toBeTruthy();
   });
+
+  it("must be not accept local part larger than 64 chars", () => {
+    const email = "e".repeat(65) + "@email.com";
+    expect(Email.validate(email)).toBeFalsy();
+  });
+
+  it("must be not accept strings larger than 320 chars", () => {
+    const email = "e".repeat(64) + "@" + "d".repeat(300) + ".com";
+    expect(Email.validate(email)).toBeFalsy();
+  });
+
+  it("must be not accept domain larger than 255 chars", () => {
+    const email = "e".repeat(64) + "@" + "d".repeat(256) + ".com";
+    expect(Email.validate(email)).toBeFalsy();
+  });
 });
